57a49d775fd23dc6d27812bd21be9a37cd964ba5
rohitydv1 <rk13332@gmail.com>
2022-07-27 00:23:23 +0530

    Updates

---
+++ precache-manifest.4eb074013197a10e01cc76da9963cc14.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babafdccb4e8205c97cb92a675201406",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "3a6b06168b6c45cb4fa0",
+    "url": "/reactjs/static/css/main.96cf40bb.chunk.css"
+  },
+  {
+    "revision": "6dcfcb3e2cb3f26049f6",
+    "url": "/reactjs/static/js/0.380f9c3c.chunk.js"
+  },
+  {
+    "revision": "02966714d00d0177804c",
+    "url": "/reactjs/static/js/1.59cbf70c.chunk.js"
+  },
+  {
+    "revision": "dcd4e8d4bd8e72a36b0e",
+    "url": "/reactjs/static/js/10.a8db886d.chunk.js"
+  },
+  {
+    "revision": "5fd5c5d675719765e58e",
+    "url": "/reactjs/static/js/11.8ca9c9d4.chunk.js"
+  },
+  {
+    "revision": "d6107a8ff68a08f61605",
+    "url": "/reactjs/static/js/4.faceda95.chunk.js"
+  },
+  {
+    "revision": "e10a04dc59f87feeb250507e93f69210",
+    "url": "/reactjs/static/js/4.faceda95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac0f324de8331b37fa2",
+    "url": "/reactjs/static/js/5.8b7d6440.chunk.js"
+  },
+  {
+    "revision": "c108b495e6715ccbc5cc",
+    "url": "/reactjs/static/js/6.e4efac22.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/reactjs/static/js/6.e4efac22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de27e5549e26ea19c8a",
+    "url": "/reactjs/static/js/7.cfeef107.chunk.js"
+  },
+  {
+    "revision": "d0f6cdb6cad08dedc944",
+    "url": "/reactjs/static/js/8.31f53177.chunk.js"
+  },
+  {
+    "revision": "1c8d312ab9db29928770",
+    "url": "/reactjs/static/js/9.1f95b4f8.chunk.js"
+  },
+  {
+    "revision": "3a6b06168b6c45cb4fa0",
+    "url": "/reactjs/static/js/main.be0200cb.chunk.js"
+  },
+  {
+    "revision": "4f4c5adebf8d234ff2a4",
+    "url": "/reactjs/static/js/runtime-main.9942f8c0.js"
+  }
+]);
